d03fe79475edf27ca651b7cfbf9142fda6829232
neimarc <neimarpersonal@gmail.com>
2022-12-05 21:23:51 -0300

    Página Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import './home-styles.css';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+
+function Home() {
+  return (
+    <div className='home-box'>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
