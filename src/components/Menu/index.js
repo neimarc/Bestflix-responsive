846daee35d0acf34de84160f184275a56cf88b57
neimarc <neimarpersonal@gmail.com>
2022-12-05 21:23:19 -0300

    Componente Menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,17 @@
+import './menu-styles.css';
+
+function Menu() {
+    return (
+        <div>
+            <ul className='menu'>
+                <li>Ação</li>
+                <li>Aventura</li>
+                <li>Comédia</li>
+                <li>Drama</li>
+                <li>Ficção</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu
