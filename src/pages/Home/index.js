bc00c73762f6144d866879f36b11be303b441a13
neimarc <neimarpersonal@gmail.com>
2022-12-06 22:23:34 -0300

    Chamada a API de filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,27 @@
 import './home-styles.css';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { useEffect, useState } from 'react';
 
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function loadMovie() {
+      const response = await fetch('https://tmdb-proxy.cubos-academy.workers.dev/3/discover/movie?language=pt-BR&include_adult=false', {
+        method: 'GET'
+      })
+
+      const { results } = await response.json();
+      setMovies(results)
+
+    }
+
+    loadMovie()
+
+  }, [])
+
   return (
     <div className='home-box'>
       <Header />
