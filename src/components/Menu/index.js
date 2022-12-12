9444c0e9d921f2ddd400a607ea8ce9aea0d24d9b
neimarc <neimarpersonal@gmail.com>
2022-12-12 17:04:12 -0300

    Adição de responsividade do Menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,8 +1,27 @@
+import { useState } from 'react';
+import CloseIcon from '../../assets/close.svg';
+import MenuIcon from '../../assets/menu.svg';
 import './menu-styles.css';
 
 function Menu() {
+    const [openMenu, setOpenMenu] = useState(false);
+
     return (
-        <div>
+        <div className={`${openMenu && 'full-menu'}`}>
+            {openMenu ?
+                <img
+                    className='close-style'
+                    style={{ display: `S{openMenu} ? 'block' : 'none'` }}
+                    src={CloseIcon}
+                    alt='close icon'
+                    onClick={() => setOpenMenu(false)} />
+                :
+                <img
+                    className='menu-style'
+                    src={MenuIcon}
+                    alt='menu icon'
+                    onClick={() => setOpenMenu(true)} />
+            }
             <ul className='menu'>
                 <li>Ação</li>
                 <li>Aventura</li>
